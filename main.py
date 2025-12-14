44f80a886d170e22d8f204317f72fcdac96c6524
Samandar2005 <samandarboriyev2005@gmail.com>
2025-12-14 19:38:33 +0500

    Feature: Run va Stop funksiyalari qo'shildi

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import docker
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI(title="DevOps Dashboard")
 client = docker.from_env() # Tizimdagi Docker bilan ulanadi
@@ -20,3 +22,32 @@ def list_containers():
         ]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+class ContainerRequest(BaseModel):
+    image: str
+    port: int
+
+@app.post("/containers/run")
+def run_container(req: ContainerRequest):
+    """Yangi konteyner ko'tarish (Masalan: nginx)"""
+    try:
+        container = client.containers.run(
+            req.image,
+            detach=True,
+            ports={'80/tcp': req.port} # Ichki 80 portni tashqi portga ulash
+        )
+        return {"message": "Konteyner ishga tushdi", "id": container.short_id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/containers/{container_id}/stop")
+def stop_container(container_id: str):
+    """Konteynerni to'xtatish"""
+    try:
+        container = client.containers.get(container_id)
+        container.stop()
+        return {"message": f"Konteyner {container_id} to'xtatildi"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
