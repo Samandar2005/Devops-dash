c39252ba578eb099f66d8f36d476c457d6a20eb9
Samandar2005 <samandarboriyev2005@gmail.com>
2025-12-15 15:52:43 +0500

    Fix: Configure WhiteNoise for static files and fix 404 errors

--- backend/containers/routing.py
+++ backend/containers/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Masalan: ws://localhost:8000/ws/containers/123/logs/
-    re_path(r'ws/containers/(?P<container_id>\w+)/logs/$', consumers.LogConsumer.as_view()),
+    # XATO: consumers.LogConsumer.as_view()
+    # TO'G'RI: consumers.LogConsumer.as_asgi()
+    re_path(r'ws/containers/(?P<container_id>\w+)/logs/$', consumers.LogConsumer.as_asgi()),
 ]
